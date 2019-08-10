007be528524a88cea61995b187b4ea5489c66fd4
qzssl <2597253199@qq.com>
2019-08-10 15:27:09 +0800

    封装jsonp和推荐页面数据请求

--- src/main.js
+++ src/main.js
@@ -3,16 +3,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
 
-import fastclick from 'fastclick' //解决300毫秒延迟
+import fastclick from 'fastclick' // 解决300毫秒延迟
 
 // Vue.config.productionTip = false;
 
 import 'common/stylus/index.styl'
 
-
-fastclick.attach(document.body);
+fastclick.attach(document.body)
 
 new Vue({
-    router,
-  render: h => h(App),
-}).$mount('#app');
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
-const resolve = dir => path.resolve(__dirname, dir);
+const path = require('path')
+const resolve = dir => path.resolve(__dirname, dir)
+
 module.exports = {
-    //部署生产环境和开发环境下的URL：可对当前环境进行区分
+    // 部署生产环境和开发环境下的URL：可对当前环境进行区分
     // publicPath:process.env.NODE_ENV === 'production'?'public': '/',
-    publicPath:'/',
+    publicPath: '/',
     /* 输出文件目录：在npm run build时，生成文件的目录名称 */
-    outputDir:'./dist',
+    outputDir: './dist',
     // 相对于outputDir的静态资源(js、css、img、fonts)目录
     assetsDir: 'assets',
     /* 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度 */
@@ -14,20 +15,21 @@ module.exports = {
     filenameHashing: false,
     /* 代码保存时进行eslint检测 */
     lintOnSave: true,
-    chainWebpack:config => {
-        //添加别名
+    chainWebpack: config => {
+        // 添加别名
         config.resolve.alias
-            .set('@',resolve('src'))
+            .set('@', resolve('src'))
             .set('common', resolve('src/common'))
             .set('components', resolve('src/components'))
             .set('router', resolve('src/router'))
             .set('base', resolve('src/base'))
-            .set('store', resolve('src/store'));
+            .set('store', resolve('src/store'))
+            .set('api', resolve('src/api'))
     },
     /* webpack-dev-server 相关配置 */
     devServer: {
         /* 自动打开浏览器 */
-        //open: true,
+        // open: true,
         /* 设置为0.0.0.0则所有的地址均能访问 */
         host: '0.0.0.0',
         port: 8080,
@@ -39,8 +41,8 @@ module.exports = {
                 /* 目标代理服务器地址 */
                 target: 'http://localhost:8080/',
                 /* 允许跨域 */
-                changeOrigin: true,
-            },
-        },
-    },
-};
+                changeOrigin: true
+            }
+        }
+    }
+}
